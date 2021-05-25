8d94adc952fa7240ad02ba2d3ec1b02476a0dfda
Ananthu <kma270999@gmail.com>
2021-05-26 01:32:42 +0530

    file for all custom exceptions here

---
+++ exceptions.py
@@ -0,0 +1,34 @@
+########## EXCEPTIONS TO BE THROWN WHEN REQUIRED PARAMETER ARE OF TYPE NONE
+class NoneValuedPars(Exception):
+    
+    def __init__(self,varname,function,message='None valued parameter(s)'):
+        self.varname = varname
+        self.function = function
+        self.message = message
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return ' %s : %s in %s'%(self.message,self.varname,self.function)
+
+########### THROWN WHEN DEFAULT ACCOUNT IS NOT SET ON THE BC CONNECTION
+class EmptyDefaultAccount(Exception):
+    
+    def __init__(self,function,message='Deafult account not set for Blockchain Connection'):
+        self.function = function
+        self.message = message
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return '%s in call %s'%(self.message,self.function)
+
+########## THROWN WHEN INCOMPATIBLE FILE TYPE IS USED OF PASSED
+class FileExtError(Exception):
+    
+    def __init__(self,required,function,message='File with in incompatible ext'):
+        self.required = required
+        self.function = function
+        self.message = message
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return ' %s :: required %s in %s'%(self.message,self.required,self.function)
