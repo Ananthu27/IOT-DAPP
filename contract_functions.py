206dc614e85763d9a201ef7769c9c9b60f7b1d90
Ananthu <kma270999@gmail.com>
2021-06-15 15:57:59 +0530

    python file for all contract related functions

---
+++ contract_functions.py
@@ -0,0 +1,13 @@
+
+
+def verifyGroupCreation(tx_receipt):
+    try:
+        return True
+    except Exception as e:
+        return False
+
+def verifyDeviceAssociation(tx_receipt):
+    try:
+        return True
+    except Exception as e:
+        return False
