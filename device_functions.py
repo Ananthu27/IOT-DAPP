c357d05030dd1f6117e937ba84a38065b7bfdb87
Ananthu <kma270999@gmail.com>
2021-06-16 13:14:38 +0530

    updated add device to group table

--- device_functions.py
+++ device_functions.py
@@ -39,17 +39,31 @@ def retrieveGroupTable():
         group_table = pd.read_json(config['data_path']+'DeviceSpecific/Device_data/group_table.json')
     return group_table
 
-def addDeviceToGroupTable(device_name):
+########## FUNCITON TO ADD DEVICE TO GROUP TABLE
+def addDeviceToGroupTable(
+    ip, port,
+    device_name,
+    pub_key,
+    master=False,
+    ):
     group_table = retrieveGroupTable()
     result = False
 
     if group_table is not None:
         try:
             device = group_table.loc[device_name]
-            print ('not possible')
             result = False
         except KeyError:
-            print ('can')
+            group_table.loc[device_name]['IP'] = ip
+            group_table.loc[device_name]['PORT'] = port
+            # group_table.loc[device_name]['DEVICE_NAME'] = device_name
+            group_table.loc[device_name]['PUB_KEY'] = pub_key
+            group_table.loc[device_name]['TIMESTAMP'] = datetime.now()
+            if master:
+                group_table.loc[device_name]['MPRECIDENCE'] = group_table['MPRECIDENCE'].max() + 1
+            else :
+                group_table.loc[device_name]['MPRECIDENCE'] = -1
+            group_table.loc[device_name]['LAST_PING'] = datetime.now()
             result = True
         finally:
             group_table.to_json(config['data_path']+'DeviceSpecific/Device_data/group_table.json')
