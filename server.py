7b985639682113eb4a6a74b1316368b76c039cb1
Ananthu <kma270999@gmail.com>
2021-06-22 13:33:40 +0530

    testing master with new group

--- server.py
+++ server.py
@@ -1,57 +1,17 @@
 ########## here blockchain default account is set based on subsciber
-# https://www.random.org/sequences/?min=10&max=20&col=10&format=plain&rnd=new
 from network import getPorts
 import socket 
 import traceback
-from crypto import getKeyPairRSA,loadKeyPairRSA
-import time
-import random
-import pickle
+from crypto import retrieveKeyPairRsa
 
-port_list = getPorts()
 host = '127.0.0.1'
-
-# master_key = input('Enter the Master key : ')
-# device_name = input('Enter device name : ')
+master = True
 new_group = True
 master_key = 'Em0C2Kv9pM'
 device_name = 'testMaster'
 
-
-def deployServer(HOST,PORT):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        # master_key = 'testing'
-        # private_key_serialized, public_key_serialized = getKeyPairRSA(serialize=True,master_key=master_key)
-        try :
-            print ('Establishing server at addr :',HOST,':',PORT)
-            s.bind((HOST, PORT))
-            # if master
-            if PORT==1111:
-                while True:
-                    message , address = s.recvfrom(1024)
-                    print ('recevied :',message,address)
-                    ########### ANSWER DIFFERNET MESSAGES HERE
-                    if message.decode() == '1':
-                        s.sendto(public_key_serialized,address)
-            # when follower
-            else:
-                while True:
-                    s.sendto('1'.encode(),('127.0.0.1',1111))
-                    message , addr = s.recvfrom(1024)
-                    discard, master_public_key = loadKeyPairRSA(message,master_key)
-                    print ('public key:',master_public_key)
-                    time.sleep(10)
-                        
-        except OSError:
-            print ('Port :',PORT,'taken')
-        except :
-            traceback.print_exc()
-        finally:
-            s.close()
-
-server = None
-
-if '1111' not in port_list and new_group:
+port_list = getPorts()
+if '1111' not in port_list and master:
     port = 1111
     
 else :
@@ -61,17 +21,22 @@ else :
             port = i
             break
 
-# deployServer(host,port)
-
 ########## this is the main frame 
 import json
 from web3 import Web3
 from blockChain import getAbiAndBytecode
 from network import getPublicPirvateIp
+from device_functions import createGroupTable, retrieveGroupTable, storeMyDevice
+from device_functions import getPublicKeyMessage, getMessage
+from contract_functions import createNewGroup
 
+########## LOADING CONFIGURATION HERE
 config = None
 with open('config.json') as f:
     config = json.load(f)
+
+########## WRITING NEW DEVICE DATA HERE
+storeMyDevice(port,device_name)
 
 ganache_url = config['ganache_endpoint']
 bcc = Web3(Web3.HTTPProvider(ganache_url))
@@ -79,34 +44,41 @@ bcc.eth.default_account = bcc.eth.accounts[1]
 abi, discard = getAbiAndBytecode(config['main_contract_path'])
 contract = bcc.eth.contract(address=config['address'],abi=abi)
 
-if new_group:
-    tx = {
-        'from':bcc.eth.default_account,
-        'value':bcc.toWei(1,'ether')
-    }
-    private_key_serialized, public_key_serialized = getKeyPairRSA(serialize=True,master_key=master_key)
-    private_ip, public_ip = getPublicPirvateIp()
+private_key_serialized, public_key_serialized = retrieveKeyPairRsa(master_key,serialize=True)
 
-    group_creation_possible = contract.functions.addGroup(
-        master_key,
-        device_name,
-        public_key_serialized.decode(),
-        ('%s:%s:%s:%s')%(public_ip,private_ip,host,port),
-        10
-    ).call(tx)
+# if this device is a master device
+if master:
+    if new_group:
 
-    if group_creation_possible:
-        tx_hash = contract.functions.addGroup(
-            master_key,
-            device_name,
-            public_key_serialized.decode(),
-            ('%s:%s:%s:%s')%(public_ip,private_ip,host,port),
-            10
-        ).transact(tx)
-        tx_receipt = bcc.eth.wait_for_transaction_receipt(tx_hash)
-        with open(config['data_path']+'DeviceSpecific/Transaction_receipt/GroupCreationReceipt','wb') as f:
-            pickle.dump(tx_receipt,f)
-        # continue infinite while loop
+        new_group_created = createNewGroup(master_key,public_key_serialized)
+        
+        if new_group_created:
+            # continue infinite while loop
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                try:
+                    s.bind((host, port))
+                    while True:
+                        # getting incoming message and message number
+                        msg, address = s.recvfrom(1024)
+                        msg = getMessage(msg)
+                        message_no = msg['message_no']
 
-    else :
-        print ('group or device already exists')
+                        ########### ANSWER DIFFERNET MESSAGES HERE
+
+                        ########### PUBLIC KEY EXCHANGE MESSAGES HANDLED HERE, MESSAGE NUMBER = 0
+                        if message_no == 0:
+                            nonce = msg['nonce']
+                            response_msg = getPublicKeyMessage(master_key,nonce)
+                            s.sendto(msg,address)
+                except OSError:
+                    print ('Port :',port,'taken')
+                except :
+                    traceback.print_exc()
+                finally:
+                    s.close()
+        # if new group cannot be created
+        else :
+            print ('group or device already exists')
+# if device is not master
+else:
+    print ('not master')
