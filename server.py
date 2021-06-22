d8c285b84221296d947af7bc8c79ba592a01e5be
Ananthu <kma270999@gmail.com>
2021-06-22 14:44:53 +0530

    checking if master is ready now and changed to common while loop

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 ########## HERE BLOCKCHAIN DEFAULT ACCOUNT IS SET BASED ON SUBSCRIBER
 
 ########## GENERAL IMPORTS
+from posix import listdir
 import socket 
 import traceback
 import json
 from web3 import Web3
+from os.path import isfile
 
 ########## USER DEFINED FUNCTION IMPORTS
 from network import getPorts
@@ -18,6 +20,7 @@ from contract_functions import createNewGroup
 host = '127.0.0.1'
 master = True
 new_group = True
+new_device = True
 master_key = 'Em0C2Kv9pM'
 device_name = 'testMaster'
 
@@ -53,37 +56,46 @@ private_key_serialized, public_key_serialized = retrieveKeyPairRsa(master_key,se
 ########## THIS IS THE MAIN FRAME
 # if this device is a master device
 if master:
+    
     if new_group:
-
         new_group_created = createNewGroup(master_key,public_key_serialized)
-        
-        if new_group_created:
-            # continue infinite while loop
-            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-                try:
-                    s.bind((host, port))
-                    while True:
-                        # getting incoming message and message number
-                        msg, address = s.recvfrom(1024)
-                        msg = getMessage(msg)
-                        message_no = msg['message_no']
-
-                        ########### ANSWER DIFFERNET MESSAGES HERE
-
-                        ########### PUBLIC KEY EXCHANGE MESSAGES HANDLED HERE, MESSAGE NUMBER = 0
-                        if message_no == 0:
-                            nonce = msg['nonce']
-                            response_msg = getPublicKeyMessage(master_key,nonce)
-                            s.sendto(msg,address)
-                except OSError:
-                    print ('Port :',port,'taken')
-                except :
-                    traceback.print_exc()
-                finally:
-                    s.close()
         # if new group cannot be created
-        else :
-            print ('group or device already exists')
+        if not new_group_created:
+            print ('GROUP ALREAD EXIST ... EXITING')
+            exit()
+
+    # check if relevant files for master is available
+    if (
+        not isfile(config['data_path']+'DeviceSpecific/Device_data/group_table.json') 
+        or not isfile(config['data_path']+'DeviceSpecific/Transaction_receipt/GroupCreationReceipt')
+    ):
+        print ('IMPORTATN FILES ARE MISSING ... EXITING')
+        exit()
+
+    # continue infinite while loop
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        try:
+            s.bind((host, port))
+            while True:
+                # getting incoming message and message number
+                msg, address = s.recvfrom(1024)
+                msg = getMessage(msg)
+                message_no = msg['message_no']
+                ########### ANSWER DIFFERNET MESSAGES HERE
+                ########### PUBLIC KEY EXCHANGE MESSAGES HANDLED HERE, MESSAGE NUMBER = 0
+                if message_no == 0:
+                    nonce = msg['nonce']
+                    response_msg = getPublicKeyMessage(master_key,nonce)
+                    s.sendto(msg,address)
+        except OSError:
+            print ('Port :',port,'taken')
+        except :
+            traceback.print_exc()
+        finally:
+            s.close()
+
 # if device is not master
 else:
+    ######### check add device here
+    ######### check ready to loop here
     print ('not master')
