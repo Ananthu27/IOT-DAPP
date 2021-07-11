4f588e82515b69d07ef41397211dbbc27da6c03c
Ananthu <kma270999@gmail.com>
2021-07-11 14:26:11 +0530

    removed while loop and replaced with recursive function

--- loopFollower.py
+++ loopFollower.py
@@ -36,105 +36,108 @@ def follower(device_object,port,logger):
             s.bind(('', port))
             logger.info('\nDEVICE RUNNING AT %s:%s'%(host,str(port)))
 
-            while True:
+        
+            ######## CREATION OF NEW DEVICE HERE
+            if device_config['new_device']:
 
-                ######## CREATION OF NEW DEVICE HERE
-                if device_config['new_device']:
-                    logger.info('\n')
-                    logger.info('\nINITIATING PUBLIC KEY EXCHANGE WITH MASTER')
-                    msg = message_object.getPublicKeyMessage(device_object,to_port=device_config['master_port'],nonce=None)
-                    s.sendto(msg,(public_ip,device_config['master_port']))
-                    msg , address = s.recvfrom(2**16)
-                    msg = message_object.getMessage(msg)
-
-                    # first check nonce of public key exchange
-                    if device_object.last_nonce[str(device_config['master_port'])] == decryptRSA(device_object.private_key,msg['nonce']):
-
-                        logger.info('\nPUBLIC KEY EXCHANGE WITH MASTER COMPLETE')
-                        logger.info('\nINITIATING DEVICE ASSOCIATION REQUEST')
-
-                        discard, to_public_key = loadKeyPairRSA(msg['public_key_serialized'],device_object.master_key)
-                        association_msg = message_object.getAssociationRequestMssg(device_object,device_config['master_port'],to_public_key)
-                        s.sendto(association_msg,(public_ip,device_config['master_port']))
-                        association_resp_msg , address = s.recvfrom(2**16)
-                        association_resp_msg = message_object.getMessage(association_resp_msg)
-                        association_resp_msg['nonce'] = decryptRSA(device_object.private_key,association_resp_msg['nonce'])
-
-                        if association_resp_msg['nonce'] == device_object.last_nonce[str(device_config['master_port'])] \
-                        and device_object.verifyGroupCreation(association_resp_msg['group_creation_tx_receipt']):
-                            logger.info('\nGROUPCREATION VERIFIED MASTER AUTHENTICATED')
-                            group_table_df = association_resp_msg['group_table']
-                            group_table_df.to_json(config['data_path']+'DeviceSpecific/Device_data/group_table.json')
-                            with open(config['data_path']+'DeviceSpecific/Transaction_receipt/GroupCreationReceipt','wb') as f:
-                                pickle.dump(association_resp_msg['group_creation_tx_receipt'],f)
-                            # updating device config
-                            device_config['new_device'] = False
-                            with open(config['data_path']+'DeviceSpecific/Device_data/device_config.json','w') as f:
-                                json.dump(device_config,fp=f,indent=5)
-                            logger.info('\nGROUPTABLE UPDATED')
-                        
-                        else:
-                            logger.info('\nSUSPECT MASTER (%s,%d)'%(address[0],address[1]))
-                            logger.info('\nDEVICE ASSOSICATION INCOMPLETE')
-                            sleep(5)
-                            s.close()
-                            follower(device_object,port,logger,device_config['master_port'])
-                    
-                    new_device = False
-
-                ######## PING MASTER HERE 
-
-                ######## CHECKING OUPBOX HERE
-                temp = listdir(config['data_path']+'DeviceSpecific/Outbox')
-                if len(temp):
-                    outbox = []
-                    for item in temp:
-                        if item.endswith('.pending.json'):
-                            outbox.append(item)
-
-                    if len(outbox):
-                        for messageName in outbox:
-                            device_object.addMessage(messageName)
-                        del outbox
-
-                    temp = listdir(config['data_path']+'DeviceSpecific/Outbox')
-                    ping = []
-                    for item in temp:
-                        if item.endswith('.pending.ping.json'):
-                            ping.append(item)
-                    
-                    if len(ping):
-                        # ping a random transaction, sleep and exit
-                        messageName = choice(ping)
-                        msg_info = None
-                        with open(messageName,'r') as f:
-                            msg_info = json.load(f)
-                        msg = message_object.getMessageTransactionPingMssg(
-                            device_object,
-                            msg_info['tx_receipt'],
-                            msg_info['message_id']
-                        )
-                        s.sendto(msg,(public_ip,msg_info['port']))
-                        # send audit trail ping to master
-                        rename(messageName,messageName.replace('.pending.ping','.completed'))
-                        sleep(5)
-
-                ######## LOOP FOR INCOMING MESSAGES UNTIL TIMEOUT
-                msg, address = s.recvfrom(2**16)
+                logger.info('\n')
+                logger.info('\nINITIATING PUBLIC KEY EXCHANGE WITH MASTER')
+                msg = message_object.getPublicKeyMessage(device_object,to_port=device_config['master_port'],nonce=None)
+                s.sendto(msg,(public_ip,device_config['master_port']))
+                msg , address = s.recvfrom(2**16)
                 msg = message_object.getMessage(msg)
-                message_no = msg['message_no']
-                logger.info('\nMESSAGE NO : %s FROM (%s,%d)'%(message_no,address[0],address[1]))
 
-                ######## ANSWER DIFFERNET MESSAGES HERE
-                
-                ######## ALIVE PING RESPONSE HANDLED HERE
-                if message_no=='4':
-                    pass
+                # first check nonce of public key exchange
+                if device_object.last_nonce[str(device_config['master_port'])] == decryptRSA(device_object.private_key,msg['nonce']):
+                    logger.info('\nPUBLIC KEY EXCHANGE WITH MASTER COMPLETE')
+                    logger.info('\nINITIATING DEVICE ASSOCIATION REQUEST')
 
-                ######## DATA_MSG TRANACTION PING MESSAGE HANDLED HERE, MESSAGE NUMBER = 5
-                elif message_no == '5':
-                    if device_object.verifyMessageTransaction(msg['tx_receipt']):
-                        device_object.getMessage(msg,address[1])
+                    discard, to_public_key = loadKeyPairRSA(msg['public_key_serialized'],device_object.master_key)
+                    association_msg = message_object.getAssociationRequestMssg(device_object,device_config['master_port'],to_public_key)
+                    s.sendto(association_msg,(public_ip,device_config['master_port']))
+                    association_resp_msg , address = s.recvfrom(2**16)
+                    association_resp_msg = message_object.getMessage(association_resp_msg)
+                    association_resp_msg['nonce'] = decryptRSA(device_object.private_key,association_resp_msg['nonce'])
+
+                    if association_resp_msg['nonce'] == device_object.last_nonce[str(device_config['master_port'])] \
+                    and device_object.verifyGroupCreation(association_resp_msg['group_creation_tx_receipt']):
+                        logger.info('\nGROUPCREATION VERIFIED MASTER AUTHENTICATED')
+                        group_table_df = association_resp_msg['group_table']
+                        group_table_df.to_json(config['data_path']+'DeviceSpecific/Device_data/group_table.json')
+                        with open(config['data_path']+'DeviceSpecific/Transaction_receipt/GroupCreationReceipt','wb') as f:
+                            pickle.dump(association_resp_msg['group_creation_tx_receipt'],f)
+                        # updating device config
+                        device_config['new_device'] = False
+                        with open(config['data_path']+'DeviceSpecific/Device_data/device_config.json','w') as f:
+                            json.dump(device_config,fp=f,indent=5)
+                        logger.info('\nGROUPTABLE UPDATED')
+                        
+                    else:
+                        logger.info('\nSUSPECT MASTER (%s,%d)'%(address[0],address[1]))
+                        logger.info('\nDEVICE ASSOSICATION INCOMPLETE')
+                        sleep(5)
+                        s.close()
+                        follower(device_object,port,logger,device_config['master_port'])
+                    
+            ######## PING MASTER HERE
+
+            ######## CHECKING OUPBOX HERE
+            temp = listdir(config['data_path']+'DeviceSpecific/Outbox')
+            if len(temp):
+                outbox = []
+                for item in temp:
+                    if item.endswith('.pending.json'):
+                        outbox.append(item)
+
+                if len(outbox):
+                    for messageName in outbox:
+                        device_object.addMessage(messageName)
+                    del outbox
+
+                temp = listdir(config['data_path']+'DeviceSpecific/Outbox')
+                ping = []
+                for item in temp:
+                    if item.endswith('.pending.ping.json'):
+                        ping.append(item)
+                    
+                if len(ping):
+                    # ping a random transaction, sleep and exit
+                    messageName = choice(ping)
+                    msg_info = None
+                    with open(messageName,'r') as f:
+                        msg_info = json.load(f)
+                    msg = message_object.getMessageTransactionPingMssg(
+                        device_object,
+                        msg_info['tx_receipt'],
+                        msg_info['message_id']
+                    )
+                    s.sendto(msg,(public_ip,msg_info['port']))
+                    # send audit trail ping to master
+                    rename(messageName,messageName.replace('.pending.ping','.completed'))
+                    sleep(5)
+
+            ######## LOOP FOR INCOMING MESSAGES UNTIL TIMEOUT
+            msg, address = s.recvfrom(2**16)
+            msg = message_object.getMessage(msg)
+            message_no = msg['message_no']
+            logger.info('\nMESSAGE NO : %s FROM (%s,%d)'%(message_no,address[0],address[1]))
+
+            ######## ANSWER DIFFERNET MESSAGES HERE
+            
+            ######## ALIVE PING RESPONSE HANDLED HERE
+            if message_no=='4':
+                pass
+
+            ######## DATA_MSG TRANACTION PING MESSAGE HANDLED HERE, MESSAGE NUMBER = 5
+            elif message_no == '5':
+                if device_object.verifyMessageTransaction(msg['tx_receipt']):
+                    device_object.getMessage(msg,address[1])
+
+            ######## ELSE LOOP AGAIN
+            else :
+                logger.warning('SKIPPING UNIDENTIFIED MESSAGE NO %s'%(message_no))
+                s.close()
+                follower(device_object,port,logger)
 
         except socket.timeout:
             logger.warning('FOLLOWER TIMEOUT.')
--- loopMaster.py
+++ loopMaster.py
@@ -26,103 +26,108 @@ def master(device_object,port,logger):
             s.bind(('', port))
             logger.info('\nMASTER RUNNING AT %s:%s'%(host,str(port)))
 
-            while True:
-                # getting incoming message and message number
-                msg, address = s.recvfrom(2**16)
-                msg = message_object.getMessage(msg)
-                message_no = msg['message_no']
-                logger.info('\nMESSAGE NO : %s FROM (%s,%d)'%(message_no,address[0],address[1]))
+            ########### CHECK OUTBOX HERE
+            temp = listdir(config['data_path']+'DeviceSpecific/Outbox')
+            if len(temp):
+                outbox = []
+                for item in temp:
+                    if item.endswith('.pending.json'):
+                        outbox.append(item)
 
-                ########### ANSWER DIFFERNET MESSAGES HERE
+                if len(outbox):
+                    for messageName in outbox:
+                        device_object.addMessage(messageName)
+                    del outbox
 
-                ########### PUBLIC KEY EXCHANGE MESSAGES HANDLED HERE, MESSAGE NUMBER = 0 (RESPONSE)
-                if message_no == '0':
-                    logger.info('\n')
-                    logger.info('\nPUBLIC KEY EXCHANGE BEGIN WITH (%s,%d)'%(address[0],address[1]))
-                    nonce = msg['nonce']
-                    discard, to_public_key = loadKeyPairRSA(msg['public_key_serialized'],device_object.master_key)
-                    response_msg = message_object.getPublicKeyMessage(device_object,to_port=None,nonce=nonce,to_public_key=to_public_key) 
-                    s.sendto(response_msg,address)
-                    logger.info('\nPUBLIC KEY EXCHANGE COMPLETE WITH (%s,%d)'%(address[0],address[1]))
-
-                ########### INCOMING ASSOCIATION REQUEST HANDLED HERE, MESSAGE NUMBER = 1 (RESPONSE)
-                elif message_no == '1':
-
-                    logger.info('\n')
-                    logger.info('\nASSOCIATION REQUEST FROM (%s,%d)'%(address[0],address[1]))
-                    association_msg = msg
-                    nonce = decryptRSA(device_object.private_key,association_msg['nonce'])
-                    discard, to_public_key = loadKeyPairRSA(association_msg['public_key_serialized'],device_object.master_key)
-
-                    # if device is authenticated 
-                    if device_object.verifyDeviceAssociation(association_msg['association_tx_receipt']):
-                        logger.info('\nVERIFIED DEVICE ASSOCIATION FOR (%s,%d)'%(address[0],address[1]))
-                        # add to group table here
-                        device_object.addDeviceToGroupTable(
-                            '%s::%s'%(private_ip,public_ip),
-                            address[1],
-                            association_msg['device_name'],
-                            association_msg['public_key_serialized'],
-                            association_msg['future_master']
-                        )
-                        logger.info('\nADDED (%s,%d) TO GROUPTABLE'%(address[0],address[1]))
-                        response_msg = message_object.getAssociationResponseMssg(device_object,nonce,to_public_key)
-                        enc_reponse_msg = response_msg
-                        s.sendto(enc_reponse_msg,address)
-                        logger.info('\nASSOCIATION RESPONSE SENT TO (%s,%d)'%(address[0],address[1]))
-                        logger.warning('\nASSOCIATION REQUEST COMPLETE WITH (%s,%d)'%(address[0],address[1]))
-
-                    else:
-                        logger.warning('\nSUSPECT (%s,%d)'%(address[0],address[1]))
-                        logger.warning('\nASSOCIATION REQUEST INCOMPLETE WITH (%s,%d)'%(address[0],address[1]))
-                
-                ########### INCOMING ALIVE PING MESSAGE
-                elif message_no == '3':
-                    pass
-
-                ########### DATA_MSG TRANACTION PING MESSAGE HANDLED HERE, MESSAGE NUMBER = 5
-                elif message_no == '5':
-                    if device_object.verifyMessageTransaction(msg['tx_receipt']):
-                        device_object.getMessage(msg,address[1])
-
-                ########### AUDIT TRAIL LOG MESSAGE
-                elif message_no == '6':
-                    pass
-
-                ########### CHECK OUTBOX HERE
                 temp = listdir(config['data_path']+'DeviceSpecific/Outbox')
-                if len(temp):
-                    outbox = []
-                    for item in temp:
-                        if item.endswith('.pending.json'):
-                            outbox.append(item)
+                ping = []
+                for item in temp:
+                    if item.endswith('.pending.ping.json'):
+                        ping.append(item)
+                
+                if len(ping):
+                    # ping a random transaction, sleep and exit
+                    messageName = choice(ping)
+                    msg_info = None
+                    with open(messageName,'r') as f:
+                        msg_info = json.load(f)
+                    msg = message_object.getMessageTransactionPingMssg(
+                        device_object,
+                        msg_info['tx_receipt'],
+                        msg_info['message_id']
+                    )
+                    s.sendto(msg,(public_ip,msg_info['port']))
+                    # self log audit trail here
+                    rename(messageName,messageName.replace('.pending.ping','.completed'))
+            
+            # CHECKING INBOX HERE
+            msg, address = s.recvfrom(2**16)
+            msg = message_object.getMessage(msg)
+            message_no = msg['message_no']
+            logger.info('\nMESSAGE NO : %s FROM (%s,%d)'%(message_no,address[0],address[1]))
 
-                    if len(outbox):
-                        for messageName in outbox:
-                            device_object.addMessage(messageName)
-                        del outbox
+            ########### ANSWER DIFFERNET MESSAGES HERE
 
-                    temp = listdir(config['data_path']+'DeviceSpecific/Outbox')
-                    ping = []
-                    for item in temp:
-                        if item.endswith('.pending.ping.json'):
-                            ping.append(item)
-                    
-                    if len(ping):
-                        # ping a random transaction, sleep and exit
-                        messageName = choice(ping)
-                        msg_info = None
-                        with open(messageName,'r') as f:
-                            msg_info = json.load(f)
-                        msg = message_object.getMessageTransactionPingMssg(
-                            device_object,
-                            msg_info['tx_receipt'],
-                            msg_info['message_id']
-                        )
-                        s.sendto(msg,(public_ip,msg_info['port']))
-                        # self log audit trail here
-                        rename(messageName,messageName.replace('.pending.ping','.completed'))
+            ########### PUBLIC KEY EXCHANGE MESSAGES HANDLED HERE, MESSAGE NUMBER = 0 (RESPONSE)
+            if message_no == '0':
+                logger.info('\n')
+                logger.info('\nPUBLIC KEY EXCHANGE BEGIN WITH (%s,%d)'%(address[0],address[1]))
+                nonce = msg['nonce']
+                discard, to_public_key = loadKeyPairRSA(msg['public_key_serialized'],device_object.master_key)
+                response_msg = message_object.getPublicKeyMessage(device_object,to_port=None,nonce=nonce,to_public_key=to_public_key) 
+                s.sendto(response_msg,address)
+                logger.info('\nPUBLIC KEY EXCHANGE COMPLETE WITH (%s,%d)'%(address[0],address[1]))
+
+            ########### INCOMING ASSOCIATION REQUEST HANDLED HERE, MESSAGE NUMBER = 1 (RESPONSE)
+            elif message_no == '1':
+
+                logger.info('\n')
+                logger.info('\nASSOCIATION REQUEST FROM (%s,%d)'%(address[0],address[1]))
+                association_msg = msg
+                nonce = decryptRSA(device_object.private_key,association_msg['nonce'])
+                discard, to_public_key = loadKeyPairRSA(association_msg['public_key_serialized'],device_object.master_key)
+
+                # if device is authenticated 
+                if device_object.verifyDeviceAssociation(association_msg['association_tx_receipt']):
+                    logger.info('\nVERIFIED DEVICE ASSOCIATION FOR (%s,%d)'%(address[0],address[1]))
+                    # add to group table here
+                    device_object.addDeviceToGroupTable(
+                        '%s::%s'%(private_ip,public_ip),
+                        address[1],
+                        association_msg['device_name'],
+                        association_msg['public_key_serialized'],
+                        association_msg['future_master']
+                    )
+                    logger.info('\nADDED (%s,%d) TO GROUPTABLE'%(address[0],address[1]))
+                    response_msg = message_object.getAssociationResponseMssg(device_object,nonce,to_public_key)
+                    enc_reponse_msg = response_msg
+                    s.sendto(enc_reponse_msg,address)
+                    logger.info('\nASSOCIATION RESPONSE SENT TO (%s,%d)'%(address[0],address[1]))
+                    logger.warning('\nASSOCIATION REQUEST COMPLETE WITH (%s,%d)'%(address[0],address[1]))
+
+                else:
+                    logger.warning('\nSUSPECT (%s,%d)'%(address[0],address[1]))
+                    logger.warning('\nASSOCIATION REQUEST INCOMPLETE WITH (%s,%d)'%(address[0],address[1]))
+                
+            ########### INCOMING ALIVE PING MESSAGE
+            elif message_no == '3':
+                pass
+
+            ########### DATA_MSG TRANACTION PING MESSAGE HANDLED HERE, MESSAGE NUMBER = 5
+            elif message_no == '5':
+                if device_object.verifyMessageTransaction(msg['tx_receipt']):
+                    device_object.getMessage(msg,address[1])
+
+            ########### AUDIT TRAIL LOG MESSAGE
+            elif message_no == '6':
+                pass
                         
+            ########### ELSE LOOP AGAIN
+            else :
+                logger.warning('CLOSING ITERATION DUE TO UNIDENTIFIED MESSAGE NO : %s'%(message_no))
+                s.close()
+                master(device_object,port,logger)
+
         except socket.timeout:
             # logger.warning('\n SERVER TIMEOUT DUE TO INACTIVITY')
             s.close()
